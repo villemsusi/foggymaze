1995301d39158245f60ff513fa1b7b5f892d73a5
villemsusi <villem.susi@gmail.com>
2023-10-17 18:39:04 +0300

    Refactored fog into a shader; Added camera delayed follow

---
+++ Assets/Scripts/Game/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float smoothSpeed = 0.09f;
+    private float z;
+
+    private void Start()
+    {
+        z = transform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, Events.GetPlayerPosition(), smoothSpeed);
+        smoothedPos.z = z;
+        transform.position = smoothedPos;
+    }
+
+
+}
