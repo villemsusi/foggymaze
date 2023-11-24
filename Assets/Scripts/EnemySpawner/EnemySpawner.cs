fe42fb0289fc4695a1496f54b6f263efbcf26869
Mihhail Mihhailov <mihhail.mihhailov@ut.ee>
2023-11-24 16:02:29 +0200

    Enemies now spawn based on your location.

--- Assets/Scripts/EnemySpawner/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner/EnemySpawner.cs
@@ -1,15 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public Object EnemyPrefab;
+    public List<GameObject> EnemyPrefabs;
     
     private float SpawnDelay;
-
     private int EnemySpawnCap;
+
+    private Tilemap tilemap;
+    private BoundsInt bounds;
+    private List<Vector3> worldLocs;
+    private Player player;
 
     void Start()
     {
+        tilemap = GameObject.Find("Ground").GetComponent<Tilemap>();
+        worldLocs = new List<Vector3>();
+        bounds = tilemap.cellBounds;
+        player = GameObject.Find("Player").GetComponent<Player>();
+
+        foreach (var pos in bounds.allPositionsWithin)
+        {
+            Vector3Int localLoc = new Vector3Int(pos.x, pos.y, pos.z);
+            if (tilemap.HasTile(localLoc))
+            {
+                Vector3 offsetPos = tilemap.CellToWorld(localLoc);
+                offsetPos.x += 0.5f;
+                offsetPos.y += 0.2f;
+                worldLocs.Add(offsetPos);
+            }
+        }
+
         EnemySpawnCap = Events.GetEnemySpawnCap();
         InvokeRepeating(nameof(SpawnEnemy), Events.GetInitialSpawnDelay(), Events.GetEnemySpawnDelay());
     }
@@ -17,6 +40,28 @@ public class EnemySpawner : MonoBehaviour
     void SpawnEnemy()
     {
         if (GameObject.FindGameObjectsWithTag("Enemy").Length < EnemySpawnCap)
-            Instantiate(EnemyPrefab, transform.position, transform.rotation, transform.parent);
+        {
+            List<Vector3> suitableWorldLocs = new List<Vector3>();
+            foreach (Vector3 loc in worldLocs)
+            {
+                if
+                (
+                    Mathf.Abs(loc.x - player.transform.position.x) >= 5 &&
+                    Mathf.Abs(loc.x - player.transform.position.x) <= 10 &&
+                    Mathf.Abs(loc.y - player.transform.position.y) >= 5 &&
+                    Mathf.Abs(loc.y - player.transform.position.y) <= 10
+                )
+                {
+                    suitableWorldLocs.Add(loc);
+                }
+            }
+
+            if (suitableWorldLocs.Count > 0)
+            {
+                int randPos = Random.Range(0, suitableWorldLocs.Count);
+                int randEnemy = Random.Range(0, EnemyPrefabs.Count);
+                GameObject newEnemy = Instantiate(EnemyPrefabs[randEnemy], suitableWorldLocs[randPos], Quaternion.identity, null);
+            }
+        }
     }
 }
